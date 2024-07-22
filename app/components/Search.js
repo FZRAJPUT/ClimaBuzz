34cd4f8eab6e44328355e79ce22d1dfed87aa37e
Subhash Kumar <142812196+FZRAJPUT@users.noreply.github.com>
2024-07-22 12:08:39 +0530

    Update Search.js

--- app/components/Search.js
+++ app/components/Search.js
@@ -27,7 +27,7 @@ const Search = () => {
     
 
   return (
-    <div className=' bb p-2 h-[80px] w-[750px] max-w-[100%] rounded-md bg-[#24242486] backdrop-blur-[80px]'>
+    <div className='main p-2 h-[80px] w-[750px] max-w-[100%] rounded-md bg-[#24242486] backdrop-blur-[80px]'>
                 <div className='flex justify-between p-2 gap-2'>
                     <input onKeyDown={handleKeyDown} value={city} onChange={(e)=>{setcity(e.target.value)}} className='max-w-[70%] rounded-[4px] p-3 outline-none bg-[#ffffff17] w-10/12 text-[#d9d7d7e9]' type='text' placeholder='Enter city' />
                     <button onClick={()=>handleInput(city)} className='bg-[#ffffff1e] px-7 rounded-[4px] text-[#ffffff9f] hover:bg-[#ffffff50] ' title='search'>Search</button>
